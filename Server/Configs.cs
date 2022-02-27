79d6a7e6c8de2b266c1703d85a69d8b0864247de
Tamashii <tamashii870@gmail.com>
2022-02-28 00:23:23 +0300

    Adapting code rof using dedicated servers...

--- Server/Configs.cs
+++ Server/Configs.cs
@@ -8,11 +8,11 @@
         public static int DelayTime { set; get; } = 0;
         public static string Endpoint { set; get; } = "/SocialAlarm";
         public static string[] URLs { set; get; } = {
-            "http://0.0.0.0:5000"};
+            "http://0.0.0.0:80"};
 
         public class Database
         {
-            public static string Host { set; get; } = "localhost";
+            public static string Host { set; get; } = "database.social-alarm.cf";
             public static string Port { set; get; } = "5432";
             public static string DatabaseName { set; get; } = "social_alarm";
             public static string Username { set; get; } = "postgres";
